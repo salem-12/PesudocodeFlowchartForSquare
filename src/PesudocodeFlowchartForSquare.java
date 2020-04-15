32585c56fe82689639dc6d315c4b549f5d648832
salem-12 <salsalem02>
2020-04-15 16:45:08 -0400

    first commit

---
+++ src/PesudocodeFlowchartForSquare.java
@@ -0,0 +1,10 @@
+public class PesudocodeFlowchartForSquare {
+    public static void main (String [] args)
+    {
+        int number, Square;
+        for (int i= 1; i <= 10; i++)
+        {
+            System.out.println(i+" "+i*i);
+        }
+}
+}
